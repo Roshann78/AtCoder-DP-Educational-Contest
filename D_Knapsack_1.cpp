8a2b61a726f355dd5eb67f4e899d5aadcccab437
Roshann78 <grd.roshan78@gmail.com>
2026-03-30 22:32:00 +0530

    knapsack 1 iterative 2d

--- D_Knapsack_1.cpp
+++ D_Knapsack_1.cpp
@@ -1,8 +1,44 @@
+/*--------------------------------------------SCHRODINGER----------------------------------------------------*/
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
 
+
+int n,w;
+vector<int>weights,value;
+vector<vector<int>>dp;
+
 void Schrodinger(){
+
+    cin>>n>>w;
+
+    weights.assign(n,0);
+    value.assign(n,0);
+    dp.assign(n,vector<int>(w+1,0));
+
+    for(int i=0;i<n;i++){
+
+        cin>>weights[i]>>value[i];
+    }
+
+
+    for(int i=weights[0];i<=w;i++){
+        dp[0][i]=value[0];
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=w;j++){
+            dp[i][j]=dp[i-1][j];
+            if(weights[i]<=j){
+                dp[i][j]=max(dp[i][j],value[i]+dp[i-1][j-weights[i]]);
+            }
+        }
+    }
+
+    cout<<dp[n-1][w];
     
 }
 
